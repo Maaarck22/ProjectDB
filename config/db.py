a0f4dc66865491d67ea05572e04db4fc23d7b698
Maaarck22 <149843489+Maaarck22@users.noreply.github.com>
2024-05-28 22:25:00 -0500

    Add files via upload

---
+++ config/db.py
@@ -0,0 +1,10 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e_commerce', 
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost', 
+        'PORT': '3306',       
+    }
+}
